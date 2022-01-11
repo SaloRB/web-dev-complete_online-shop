9ea144a1d27712ecf68466906ab1ddf69676eeca
Salomon Romano <salo_romano@yahoo.com>
2022-01-11 16:35:04 -0600

    Updated DOM after cart updates

--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,5 +1,5 @@
 const addToCartButtonElement = document.querySelector("#product-details button")
-const cartBadgeElement = document.querySelector(".nav-items .badge")
+const cartBadgeElements = document.querySelectorAll(".nav-items .badge")
 
 async function addToCart() {
   const productId = addToCartButtonElement.dataset.productid
@@ -11,7 +11,7 @@ async function addToCart() {
       method: "POST",
       body: JSON.stringify({
         productId,
-        _csrf: csrfToken
+        _csrf: csrfToken,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -31,7 +31,9 @@ async function addToCart() {
 
   const newTotalQuantity = responseData.newTotalItems
 
-  cartBadgeElement.textContent = newTotalQuantity
+  cartBadgeElements.forEach((badge) => {
+    badge.textContent = newTotalQuantity
+  })
 }
 
 addToCartButtonElement.addEventListener("click", addToCart)
